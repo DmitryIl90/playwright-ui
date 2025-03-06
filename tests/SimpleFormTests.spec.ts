6c2f1467dd9f87acf9ae55a60d41c46e0e144894
Dmitry Ilves <ilvesdmitry@gmail.com>
2025-03-06 22:10:35 +0200

    feat: HW-16 added tests

--- tests/SimpleFormTests.spec.ts
+++ tests/SimpleFormTests.spec.ts
@@ -1,33 +1,44 @@
 import { test, expect } from "@playwright/test";
-import exp = require("node:constants");
 
 test.describe("Local simple form tests", async () => {
     test.beforeEach(async ({ page }) => {
         const path = require('path');
-        const filePath = `file://${path.resolve('src/simpleForm.html' )}`;
-        await page.goto(filePath);
+        const BaseURL = `file://${path.resolve(process.env.BaseURL)}`;
+        await page.goto(BaseURL );
     })
 
     test('Form opens', async ({ page }) => {
         // locators
-        const emailField = page.getByTestId("email");
-        const usernameField = page.getByTestId("username");
-        const submitButton = page.getByTestId("submit-order");
-        const popupMessage = page.locator("#popup-message");
+        const loginField = page.getByTestId("username-input");
+        const passwordField = page.getByTestId("password-input");
+        const signInButton = page.getByTestId("signIn-button");
+        const inputError = page.getByTestId("username-input-error");
+        const popupMessage = page.getByTestId("authorizationError-popup");
+        const popupCloseButton = page.getByTestId("authorizationError-popup-close-button");
 
         // actions
-        await expect(emailField).toBeVisible();
-        await expect(usernameField).toBeVisible();
-        await expect(submitButton).toBeVisible();
-        await expect(submitButton).toBeDisabled();
-        await usernameField.fill("testusername");
-        await expect(submitButton).toBeDisabled();
-        await emailField.fill("testestesttest@testest.test");
-        await expect(submitButton).toBeEnabled();
+        await expect(loginField).toBeVisible();
+        await expect(passwordField).toBeVisible();
+        await expect(signInButton).toBeVisible();
+        await expect(inputError).not.toBeVisible();
+        await expect(signInButton).toBeDisabled();
+        await loginField.fill("123")
+        await passwordField.fill("testestest");
+        await expect(signInButton).toBeEnabled();
         await expect(popupMessage).not.toBeVisible();
-        await submitButton.click();
+        await expect(popupCloseButton).not.toBeVisible();
+        await signInButton.click();
         await expect(popupMessage).toBeVisible();
-        await page.waitForTimeout(5100);
+        await expect(popupCloseButton).toBeVisible();
+        await popupCloseButton.click();
         await expect(popupMessage).not.toBeVisible();
+        await expect(popupCloseButton).not.toBeVisible();
+        await loginField.fill("");
+        await expect(inputError).toBeVisible();
+        await loginField.fill("123");
+        await expect(inputError).not.toBeVisible();
+        await passwordField.fill("");
+        await expect(inputError).toBeVisible();
+        await expect(signInButton).toBeDisabled();
     });
 })
